f232c9b0410e75f7970f52d9972c1559b2d81001
Samadhi-12 <sama.gunasena12@gmail.com>
2025-05-22 02:18:48 +0530

    change main activity_main.xml add drawable bg_style.xml create drawable for equal button as equal_btn.xml change equal button layout add string file for symbols in buttons add styles for buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.lottie)
+    implementation ("net.objecthunter:exp4j:0.4.8")
 }
